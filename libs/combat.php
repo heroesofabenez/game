879556562673b60f17120c5434bd96c1093c1a54
konecnyjakub <konecnyjakub@61c07b97-58b8-4008-9326-805ca56ad521>
2015-03-27 17:24:45 +0000

    added class Team
    git-svn-id: https://svn.riouxsvn.com/heroesofabenez/trunk@10 61c07b97-58b8-4008-9326-805ca56ad521

--- libs/combat.php
+++ libs/combat.php
@@ -1,5 +1,20 @@
 <?php
 if(MASTER_ID !== "HEROES_OF_ABENEZ") exit;
+
+class Team extends Object {
+  private $name;
+  private $members = array();
+  function __construct($name) {
+    if(!is_string($name)) { exit("Invalid value for parameter name passed to method Team::__construct. Expected string."); }
+    else ( $this->$name = $name; )
+  }
+  
+  function addMember($member) {
+    if(!is_a($member, "Character")) { exit("Invalid value for parameter member passed to method Team:addMember. Expected Character."); }
+    else { $this->members[] = $member; }
+  }
+}
+
 class CombatBase extends Object {
   private $team1 = array();
   private $team2 = array();
