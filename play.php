e521c24ee63ab895585a1c690c4dd4ced80e1686
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-03 20:35:07 +0200

    added testing configuration file, changed $base_url for testing

--- play.php
+++ play.php
@@ -1,6 +1,6 @@
 <?php
 define('MASTER_ID', 'HEROES_OF_ABENEZ');
-$base_url = "http://heroesofabenez.tk";
+$base_url = "http://kobliha/heroesofabenez/play.php";
 
 define('WWW_DIR', dirname(__FILE__));
 define('APP_DIR', WWW_DIR . '/app');
