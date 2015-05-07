8e295c35334abce180bda870de16126a3de1c8c6
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-07 17:08:41 +0200

    return player's identity in Authenticator

--- app/model/Authenticator.php
+++ app/model/Authenticator.php
@@ -2,13 +2,31 @@
 use Nette\Security as NS;
 
 class Authenticator extends Nette\Object implements NS\IAuthenticator {
-  function __construct() { }
+  public $db;
+  function __construct(Nette\Database\Context $database) {
+    $this->db = $database;
+  }
   
   function authenticate(array $credentials) {
     $dev_servers = array("localhost", "kobliha", "test.heroesofabenez.tk");
     if(in_array($_SERVER["SERVER_NAME"], $dev_servers)) {
-      return new NS\Identity(1, "admin");
+      $id = 1;
     } else { }
+    $char = $this->db->table("characters")->get($id);
+    $race = $this->db->table("character_races")->get($char->race);
+    $occupation = $this->db->table("character_classess")->get($char->occupation);
+    if(is_int($char->specialization)) {
+      $specialization = $this->db->table("character_specializations")->get($char->specialization)->name;
+    } else {
+      $specialization = null;
+    }
+    $specialization = $this->db->table("character_specializations")->get($char->specialization);
+    $data = array(
+      "name" => $char->name, "race" => $race->name, "gender" => $char->gender,
+      "occupation" => $occupation->name, "specialization" => $specialization,
+      "level" => $char->level, "guild" => $char->guild,
+    );
+    return new NS\Identity($id, $char->rank->name, $data);
   }
 }
 ?>
