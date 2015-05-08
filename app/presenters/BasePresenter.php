1950805d70518d6edac00fc50f584378e635525f
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-08 14:41:26 +0200

    fixed BasePresenter::tryLogin() for Character:create, do not allow creating character if player already has one, tweaks to redirects in BasePresenter::tryLogin()

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -16,12 +16,14 @@ class BasePresenter extends Nette\Application\UI\Presenter {
     $user =$this->context->getService("user");
     /*if(!$user->isLoggedIn())*/ $user->login();
     $uid = $this->user->identity->id;
+    if(is_a($this->presenter, "CharacterPresenter") AND $uid == -1) return;
+    if(is_a($this->presenter, "CharacterPresenter") AND $uid > 0) $this->redirect(301, "Homepage:default");;
     switch($uid) {
 case -1:
-  $this->redirect("Character:create");
+  $this->redirect(302, "Character:create");
   break;
 case 0:
-  //$this->redirect("http://heroesofabenez.tk/");
+  //$this->redirectUrl("http://heroesofabenez.tk/");
     }
   }
 }
