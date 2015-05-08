53bffe2d765b34f60f7810bb87ef73a72bd4d390
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-08 12:16:43 +0200

    redirect player if he is not logged in/has no character

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -2,8 +2,7 @@
 class BasePresenter extends Nette\Application\UI\Presenter {
   function startup() {
     parent::startup();
-    $user =$this->context->getService("user");
-    if(!$user->isLoggedIn()) $user->login();
+    $this->tryLogin();
     $this->template->server = $this->context->parameters["application"]["server"];
   }
   
@@ -13,4 +12,18 @@ class BasePresenter extends Nette\Application\UI\Presenter {
     return $db;
   }
   
+  function tryLogin() {
+    $user =$this->context->getService("user");
+    /*if(!$user->isLoggedIn())*/ $identity = $user->login();
+    $uid = $this->user->identity->id;
+    switch($uid) {
+case -1:
+  echo "You have no character on this server. Create one now.";
+  $this->redirect("Character:create");
+  break;
+case 0:
+  echo "You are not logged in. Go to the website, login there and return.";
+  //$this->redirect("http://heroesofabenez.tk/");
+    }
+  }
 }
