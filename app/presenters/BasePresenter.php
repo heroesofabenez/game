cd472ea192ed4a64e6bd74d1a40718b85e5f3608
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-08 15:39:56 +0200

    removed a redundant semicolon from BasePresenter

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -17,7 +17,7 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter {
     /*if(!$user->isLoggedIn())*/ $user->login();
     $uid = $this->user->identity->id;
     if(is_a($this->presenter, "CharacterPresenter") AND $uid == -1) return;
-    if(is_a($this->presenter, "CharacterPresenter") AND $uid > 0) $this->redirect(301, "Homepage:default");;
+    if(is_a($this->presenter, "CharacterPresenter") AND $uid > 0) $this->redirect(301, "Homepage:default");
     switch($uid) {
 case -1:
   $this->redirect(302, "Character:create");
