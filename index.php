7985a75ee77bc1af9edc32358ec8b1a01d1670ff
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-09 21:04:38 +0200

    removed no longer needed constant TEMPLATES_DIR

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ define("MASTER_ID", "HEROES_OF_ABENEZ");
 define("WWW_DIR", dirname(__FILE__));
 define("APP_DIR", WWW_DIR . "/app");
 define("LIBS_DIR", WWW_DIR . "/libs");
-define("TEMPLATES_DIR", APP_DIR . "/templates");
 
 require APP_DIR . "/bootstrap.php";
 ?>
