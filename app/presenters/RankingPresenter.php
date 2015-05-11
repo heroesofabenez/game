c1784d584fda9a8a3a68b75fd5ccf0158734e7dd
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-11 15:09:55 +0200

    added some documentation comments

--- app/presenters/RankingPresenter.php
+++ app/presenters/RankingPresenter.php
@@ -5,10 +5,18 @@
    * @author Jakub Konečný
    */
 class RankingPresenter extends BasePresenter {
+  /**
+   * @todo do pagination
+   * @return void
+   */
   function renderDefault() {
     $this->template->characters = Ranking::characters($this->db);
   }
   
+  /**
+   * @todo do pagination and ordering
+   * @return void
+   */
   function renderGuilds() {
     $this->template->guilds = Ranking::guilds($this->db);
   }
