076d6ad1e2f46ef5efabb1c7ce6ec5aa9698edcd
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-12 17:31:45 +0200

    moved routes to config.neon, moved server-specific settings to config.local.neon (for now it is database parameters, server name and timezone]

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,8 +2,6 @@
 require LIBS_DIR . '/nette.phar';
 Tracy\Debugger::enable(null, APP_DIR . '/log');
 
-date_default_timezone_set("Europe/Prague");
-
 $configurator = new Nette\Configurator;
 $configurator->setTempDirectory(APP_DIR . "/temp");
 $configurator->addConfig(APP_DIR . '/config.neon');
@@ -12,20 +10,6 @@ $configurator->createRobotLoader()
     ->addDirectory(APP_DIR)
     ->register();
 $container = $configurator->createContainer();
-
-use Nette\Application\Routers\RouteList,
-    Nette\Application\Routers\Route;
-
-$router = new RouteList;
-$router[] = new Route("profile/<id>", "Profile:view");
-$router[] = new Route("guild/join[/<id>]", "Guild:join");
-$router[] = new Route("guild/create", "Guild:create");
-$router[] = new Route("guild/<id>", "Guild:view");
-$router[] = new Route("travel[/<location>]", "Travel:default");
-$router[] = new Route("ranking/guilds[/<page>]", "Ranking:guilds");
-$router[] = new Route("ranking[/<page>]", "Ranking:default");
-$router[] = new Route("<presenter>[/<action>][/<id>]", "Homepage:default");
-$container->addService("router", $router);
 
 $container->getService("application")->run();
 ?>
