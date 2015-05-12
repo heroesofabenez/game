0ff312f7455b1aa345db64c9243f6750ada1038f
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-12 14:27:27 +0200

    error presenter now distinguishes between "not found" and other errors, renamed template Error\default to 404, added template Error\500 (taken from Nette but it uses site design)

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -5,5 +5,11 @@
  * @author Jakub Konečný
  */
 class ErrorPresenter extends BasePresenter {
-  
+  function actionDefault($exception) {
+    if($exception instanceof Nette\Application\BadRequestException) {
+      $this->setView("404");
+    } else {
+      $this->setView("500");
+    }
+  }
 }
