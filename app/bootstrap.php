b4fbfceac56f8e31bd4ad1fed382f96731f7cfc2
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-13 22:32:25 +0200

    moved config files into sub folder config, tweaked their name config.neone -> main.neon, config.local.neon -> local.neon

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ Tracy\Debugger::enable(null, APP_DIR . "/log");
 
 $configurator = new Nette\Configurator;
 $configurator->setTempDirectory(APP_DIR . "/temp");
-$configurator->addConfig(APP_DIR . "/config.neon");
+$configurator->addConfig(APP_DIR . "/config/main.neon");
 $configurator->createRobotLoader()
     ->addDirectory(LIBS_DIR)
     ->addDirectory(APP_DIR)
