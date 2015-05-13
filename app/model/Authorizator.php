43c5a3518eee8704a0f7a4ec9a89d450317e6126
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-14 00:15:15 +0200

    Authorizator::create() no longer access cache, it is left to getters

--- app/model/Authorizator.php
+++ app/model/Authorizator.php
@@ -10,22 +10,34 @@ class Authorizator extends \Nette\Object {
   /**
    * Get roles from db
    * 
-   * @param \Nette\Database\Context $db
+   * @param \Nette\Di\Container $container
    * @return array
    */
-  static function getRoles(\Nette\Database\Context $db) {
-    $rolesRows = $db->table("guild_ranks")->order("id");
-    foreach($rolesRows as $i => $row) {
-      $roles[$i] = array("id" => $row->id, "name" => $row->name);
+  static function getRoles(\Nette\Di\Container $container) {
+    $cache = $container->getService("caches.permissions");
+    $roles = $cache->load("roles");
+    if($roles === NULL) {
+      $db = $container->getService("database.default.context");
+      $rolesRows = $db->table("guild_ranks")->order("id");
+      foreach($rolesRows as $i => $row) {
+        $roles[$i] = array("id" => $row->id, "name" => $row->name);
+      }
+      $cache->save("roles", $roles);
     }
     return $roles;
   }
   
-  static function getPermissions(\Nette\Database\Context $db, $roles) {
-    $privileges = $db->table("guild_privileges");
-    foreach($privileges as $row) {
-      $role = $roles[$row->rank];
-      $permissions[$row->id] = array("role" => $role["name"], "action" => $row->action);
+  static function getPermissions(\Nette\Di\Container $container, $roles) {
+    $cache = $container->getService("caches.permissions");
+    $permissions = $cache->load("permissions");
+    if($permissions === NULL) {
+      $db = $container->getService("database.default.context");
+      $privileges = $db->table("guild_privileges");
+      foreach($privileges as $row) {
+        $role = $roles[$row->rank];
+        $permissions[$row->id] = array("role" => $role["name"], "action" => $row->action);
+      }
+      $cache->save("permissions", $permissions);
     }
     return $permissions;
   }
@@ -35,21 +47,12 @@ class Authorizator extends \Nette\Object {
   */
   static function create(\Nette\Di\Container $container) {
     $permission = new \Nette\Security\Permission;
+    $permission->addResource("guild");
     $permission->addRole("guest");
     $permission->addRole("player", "guest");
     
-    $cache = $container->getService("caches.permissions");
-    $db = $container->getService("database.default.context");
-    $roles = $cache->load("roles");
-    $permissions = $cache->load("permissions");
-    $permission->addResource("guild");
-    
-    if($roles === NULL OR $permissions === NULL) {
-      $roles = Authorizator::getRoles($db);
-      $permissions = Authorizator::getPermissions($db, $roles);
-      $cache->save("roles", $roles);
-      $cache->save("permissions", $permissions);
-    }
+    $roles = Authorizator::getRoles($container);
+    $permissions = Authorizator::getPermissions($container, $roles);
     
     foreach($roles as $i => $row) {
       if($row["id"] == 1) {
