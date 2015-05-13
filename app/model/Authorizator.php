e031274ce91e8961010c44898e83fc70b2f44657
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-13 20:43:35 +0200

    cache data for Authorizator, moved their getting from db into separate functions

--- app/model/Authorizator.php
+++ app/model/Authorizator.php
@@ -8,29 +8,61 @@ namespace HeroesofAbenez;
  */
 class Authorizator extends \Nette\Object {
   /**
-  * @return \Nette\Security\Permission
-  */
-  static function create(\Nette\Database\Context $db) {
-    $permission = new \Nette\Security\Permission;
-    $permission->addRole("guest");
-    $permission->addRole("player", "guest");
-    $roles = $db->table("guild_ranks")->order("id");
-    foreach($roles as $i => $row) {
-      if($row->id == 1) {
-        $parent = "player";
-      } else {
-        $parentRow = $roles[$i-1];
-        $parent = $parentRow->name;
-      }
-      $permission->addRole($row->name, $parent);
+   * Get roles from db
+   * 
+   * @param \Nette\Database\Context $db
+   * @return array
+   */
+  static function getRoles(\Nette\Database\Context $db) {
+    $rolesRows = $db->table("guild_ranks")->order("id");
+    foreach($rolesRows as $i => $row) {
+      $roles[$i] = array("id" => $row->id, "name" => $row->name);
     }
-    
-    $permission->addResource("guild");
-    
+    return $roles;
+  }
+  
+  static function getPermissions(\Nette\Database\Context $db, $roles) {
     $privileges = $db->table("guild_privileges");
     foreach($privileges as $row) {
       $role = $roles[$row->rank];
-      $permission->allow($role->name, "guild", $row->action);
+      $permissions[$row->id] = array("role" => $role["name"], "action" => $row->action);
+    }
+    return $permissions;
+  }
+  
+  /**
+  * @return \Nette\Security\Permission
+  */
+  static function create(\Nette\Di\Container $container) {
+    $permission = new \Nette\Security\Permission;
+    $permission->addRole("guest");
+    $permission->addRole("player", "guest");
+    
+    $cache = $container->getService("permissionsCache");
+    $db = $container->getService("database.default.context");
+    $roles = $cache->load("roles");
+    $permissions = $cache->load("permissions");
+    $permission->addResource("guild");
+    
+    if($roles === NULL OR $permissions === NULL) {
+      $roles = Authorizator::getRoles($db);
+      $permissions = Authorizator::getPermissions($db, $roles);
+      $cache->save("roles", $roles);
+      $cache->save("permissions", $permissions);
+    }
+    
+    foreach($roles as $i => $row) {
+      if($row["id"] == 1) {
+        $parent = "player";
+      } else {
+        $parentRow = $roles[$i-1];
+        $parent = $parentRow["name"];
+      }
+      $permission->addRole($row["name"], $parent);
+    }
+    
+    foreach($permissions as $row) {
+      $permission->allow($row["role"], "guild", $row["action"]);
     }
     return $permission;
   }
