37d0f5075a770e4f37f7d4929e734d0911385a82
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-25 13:26:33 +0200

    added logging to ErrorPresenter

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -1,17 +1,27 @@
 <?php
 namespace HeroesofAbenez\Presenters;
 
+use Tracy\ILogger;
+
 /**
  * Presenter Error
  *
  * @author Jakub Konečný
  */
 class ErrorPresenter extends BasePresenter {
+  /** @var ILogger|NULL */
+  private $logger;
+  
+  function __construct(ILogger $logger = NULL) {
+    $this->logger = $logger;
+  }
+  
   function actionDefault($exception) {
     if($exception instanceof \Nette\Application\BadRequestException) {
       $this->setView("404");
     } else {
       $this->setView("500");
+      if($this->logger) $this->logger->log($exception, ILogger::EXCEPTION);
     }
   }
 }
