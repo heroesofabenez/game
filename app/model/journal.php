e24475746759f4423c820abf69b2b70e33d1760b
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-05-31 18:09:31 +0200

    shortened method Journal::basic()

--- app/model/journal.php
+++ app/model/journal.php
@@ -26,31 +26,25 @@ class Journal {
    * @return array
    */
   static function basic(\Nette\DI\Container $container) {
-    $return = array();
-    $user = $container->getService("security.user");
-    $return["name"] = $user->identity->name;
-    $return["gender"] = $user->identity->gender;
-    $return["race"] = $user->identity->race;
-    $return["occupation"] = $user->identity->occupation;
-    $return["specialization"] = $user->identity->specialization;
-    $return["level"] = $user->identity->level;
-    $return["whiteKarma"] = $user->identity->white_karma;
-    $return["neutralKarma"] = $user->identity->neutral_karma;
-    $return["darkKarma"] = $user->identity->dark_karma;
+    $user = $container->getService("security.user")->identity;
     $db = $container->getService("database.default.context");
     $character = $db->table("characters")->get($user->id);
-    $return["experiences"] = $character->experience;
-    $return["description"] = $character->description;
-    if($user->identity->guild > 0) {
-      $return["guild"] = GuildModel::getGuildName($user->identity->guild, $container);
-      $return["guildRank"] = ucfirst($user->identity->roles[0]);
+    $stages = Location::listOfStages($container);
+    $stage = $stages[$user->stage];
+    $return = array(
+      "name" => $user->name, "gender" => $user->gender, "race" => $user->race,
+      "occupation" => $user->occupation, "specialization" => $user->specialization,
+      "level" => $user->level, "whiteKarma" => $user->white_karma,
+      "neutralKarma" => $user->neutral_karma, "darkKarma" => $user->dark_karma,
+      "experiences" => $character->experience, "description" => $character->description,
+      "stageName" => $stage->name, "areaName" => Location::getAreaName($stage->area, $container)
+    );
+    if($user->guild > 0) {
+      $return["guild"] = GuildModel::getGuildName($user->guild, $container);
+      $return["guildRank"] = ucfirst($user->roles[0]);
     } else {
       $return["guild"] = false;
     }
-    $stages = Location::listOfStages($container);
-    $stage = $stages[$user->identity->stage];
-    $return["stageName"] = $stage->name;
-    $return["areaName"] = Location::getAreaName($stage->area, $container);
     return $return;
   }
   
