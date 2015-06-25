4af9d419cd98ace99d64593b74e7d1f962e64384
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-06-26 01:36:41 +0200

    do not store location model in GlobalChatControl (it is not necessary)

--- app/components/GlobalChatControl.php
+++ app/components/GlobalChatControl.php
@@ -7,18 +7,14 @@ namespace HeroesofAbenez\Chat;
  * @author Jakub Konečný
  */
 class GlobalChatControl extends ChatControl {
-  /** @var \HeroesofAbenez\Location */
-  protected $locationModel;
-  
   /**
    * @param \Nette\Database\Context $database
    * @param \Nette\Security\User $user
    * @param \HeroesofAbenez\Location $locationModel
    */
   function __construct(\Nette\Database\Context $database, \Nette\Security\User $user, \HeroesofAbenez\Location $locationModel) {
-    $this->locationModel = $locationModel;
-    $stage = $this->locationModel->getStage($user->identity->stage);
-    $stages = $this->locationModel->listofStages($this->id);
+    $stage = $locationModel->getStage($user->identity->stage);
+    $stages = $locationModel->listofStages($this->id);
     $stagesIds = array();
     foreach($stages as $s) {
       $stagesIds[] = $s->id;
