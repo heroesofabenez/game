d64a178bb6a956b1aaaeb7dd6820791e3e7252f2
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-06-26 19:27:31 +0200

    removed no longer needed method Ranking::guild()

--- app/model/ranking.php
+++ app/model/ranking.php
@@ -38,22 +38,5 @@ class Ranking extends \Nette\Object {
     }
     return $chars;
   }
-  
-  /**
-   * Gets list of all guilds
-   * @return array List of all guilds (name, number of members)
-   */
-  function guilds() {
-    $return = array();
-    $result = $this->guildModel->listOfGuilds();
-    foreach($result as $row) {
-      $data[] = (array) $row;
-    }
-    $data2 = Utils\Arrays::orderby($data, "members", SORT_DESC, "id", SORT_ASC);
-    foreach($data2 as $row2) {
-      $return[] = new Guild($row2["id"], $row2["name"], "", $row2["members"]);
-    }
-    return $return;
-  }
 }
 ?>
