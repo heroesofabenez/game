3bed943fd698674f3f563126558a591b7519184b
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-07-22 21:41:27 +0200

    get Nette Framework via Composer

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,14 +2,13 @@
 use Nette\Application\Routers\RouteList,
     Nette\Application\Routers\Route;
 
-require LIBS_DIR . "/nette.phar";
+require WWW_DIR . "/vendor/autoload.php";
 Tracy\Debugger::enable(null, APP_DIR . "/log");
 
 $configurator = new Nette\Configurator;
 $configurator->setTempDirectory(APP_DIR . "/temp");
 $configurator->addConfig(APP_DIR . "/config/main.neon");
 $configurator->createRobotLoader()
-    ->addDirectory(LIBS_DIR)
     ->addDirectory(APP_DIR)
     ->register();
 $container = $configurator->createContainer();
