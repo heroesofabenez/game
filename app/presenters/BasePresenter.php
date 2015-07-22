a944d8049e9a93cd8b3019ebaead4566442eff9e
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-07-23 00:56:59 +0200

    simplified BasePresenter::tryLogin()

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -27,10 +27,10 @@ abstract class BasePresenter extends \Nette\Application\UI\Presenter {
   function tryLogin() {
     if(!$this->user->isLoggedIn()) $this->user->login();
     $uid = $this->user->id;
-    if(is_a($this->presenter, "\HeroesofAbenez\Presenters\CharacterPresenter") AND $uid == -1) return;
-    if(is_a($this->presenter, "\HeroesofAbenez\Presenters\CharacterPresenter") AND $this->user->identity->stage == NULL) return;
-    if(is_a($this->presenter, "\HeroesofAbenez\Presenters\CharacterPresenter") AND $uid > 0) $this->redirect(301, "Homepage:default");
-    if(is_a($this->presenter, "\HeroesofAbenez\Presenters\IntroPresenter") AND $this->user->identity->stage == NULL) return;
+    if($this->presenter instanceof CharacterPresenter AND $uid == -1) return;
+    if($this->presenter instanceof CharacterPresenter AND $this->user->identity->stage == NULL) return;
+    if($this->presenter instanceof CharacterPresenter AND $uid > 0) $this->redirect(301, "Homepage:default");
+    if($this->presenter instanceof IntroPresenter AND $this->user->identity->stage == NULL) return;
     switch($uid) {
 case -1:
   $this->redirect(302, "Character:create");
