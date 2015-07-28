ffe6f9885435424c973805cf5f6b73468b029906
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-07-28 20:16:10 +0200

    created basic stuff for arena

---
+++ app/presenters/ArenaPresenter.php
@@ -0,0 +1,20 @@
+<?php
+namespace HeroesofAbenez\Presenters;
+
+/**
+ * Presenter Arena
+ *
+ * @author Jakub Konečný
+ */
+class ArenaPresenter extends BasePresenter {
+  /** @var int */
+  protected $min_level = 3;
+  
+  /**
+   * @return void
+   */
+  function actionDefault() {
+    
+  }
+}
+?>
