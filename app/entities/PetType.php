574b0552652496aa724c91f43ade5404b9c793d2
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-08-29 14:13:39 +0200

    added documentation comments for entity PetType

--- app/entities/PetType.php
+++ app/entities/PetType.php
@@ -9,14 +9,23 @@ namespace HeroesofAbenez\Entities;
  * @author Jakub Konečný
  */
 class PetType extends BaseEntity {
+  /** @var int */
   protected $id;
+  /** @var string */
   protected $name;
+  /** @var string */
   protected $bonus_stat;
+  /** @var int */
   protected $bonus_value;
+  /** @var string */
   protected $image;
+  /** @var int */
   protected $required_level;
+  /** @var int */
   protected $required_class;
+  /** @var int */
   protected $required_race;
+  /** @var int */
   protected $cost;
   
   function __construct(\Nette\Database\Table\ActiveRow $row) {
@@ -40,6 +49,9 @@ class PetType extends BaseEntity {
     return $this->bonus_value;
   }
   
+  /**
+   * @return string
+   */
   function __toString() {
     return $this->name;
   }
