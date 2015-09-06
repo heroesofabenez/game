d5abc966ec00093cdcb67f6a5eb1624833a04e08
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-09-06 13:40:53 +0200

    changed $this->presenter to just $this in BasePresenter::tryLogin()

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -32,10 +32,10 @@ abstract class BasePresenter extends \Nette\Application\UI\Presenter {
   function tryLogin() {
     if(!$this->user->isLoggedIn()) $this->user->login();
     $uid = $this->user->id;
-    if($this->presenter instanceof CharacterPresenter AND $uid == -1) return;
-    if($this->presenter instanceof CharacterPresenter AND $this->user->identity->stage == NULL) return;
-    if($this->presenter instanceof CharacterPresenter AND $uid > 0) $this->redirect(301, "Homepage:default");
-    if($this->presenter instanceof IntroPresenter AND $this->user->identity->stage == NULL) return;
+    if($this instanceof CharacterPresenter AND $uid == -1) return;
+    if($this instanceof CharacterPresenter AND $this->user->identity->stage == NULL) return;
+    if($this instanceof CharacterPresenter AND $uid > 0) $this->redirect(301, "Homepage:default");
+    if($this instanceof IntroPresenter AND $this->user->identity->stage == NULL) return;
     switch($uid) {
 case -1:
   $this->redirect(302, "Character:create");
