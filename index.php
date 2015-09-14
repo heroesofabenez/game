a4570e951f46ee34186b4d5c0990deb7f107ca83
Jakub Konečný <jakub.konecny2@centrum.cz>
2015-09-14 18:32:37 +0200

    got rid of constant LIBS_DIR (it is no longer needed)

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 define("WWW_DIR", dirname(__FILE__));
 define("APP_DIR", WWW_DIR . "/app");
-define("LIBS_DIR", WWW_DIR . "/libs");
 
 $container = require APP_DIR . "/bootstrap.php";
 $container->getService("application")->run();
