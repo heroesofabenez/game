c7b9baf7592f0eab35373c86242d1ff2747e8cd6
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-07-13 13:01:05 +0200

    use just 1 bootstrap and add mytester only in debug mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,7 @@ Tracy\Debugger::enable(null, APP_DIR . "/log");
 $configurator = new Nette\Configurator;
 $configurator->setTempDirectory(APP_DIR . "/temp");
 $configurator->addConfig(APP_DIR . "/config/main.neon");
+if($configurator->isDebugMode()) $configurator->addConfig(APP_DIR . "/config/mytester.neon");
 $configurator->createRobotLoader()
     ->addDirectory(APP_DIR)
     ->register();
--- run_tests.php
+++ run_tests.php
@@ -6,13 +6,6 @@ define("TESTER_DIR", WWW_DIR . "/libs/mytester/src");
 require __DIR__ . "/vendor/autoload.php";
 Tracy\Debugger::enable(null, APP_DIR . "/log");
 
-$configurator = new Nette\Configurator;
-$configurator->setTempDirectory(APP_DIR . "/temp");
-$configurator->addConfig(APP_DIR . "/config/main.neon");
-$configurator->addConfig(APP_DIR . "/config/mytester.neon");
-$robotLoader = $configurator->createRobotLoader();
-$robotLoader->addDirectory(APP_DIR);
-$robotLoader->register();
-$container = $configurator->createContainer();
+$container = require APP_DIR . "/bootstrap.php";
 $container->getService("mytester.runner")->execute();
 ?>
