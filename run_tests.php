4dde4a87fb4713f2124a40d92dc2c949d64b15a3
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-07-14 18:14:12 +0200

    removed unneeded constant TESTER_DIR in run_tests.php

--- run_tests.php
+++ run_tests.php
@@ -1,7 +1,6 @@
 <?php
 define("WWW_DIR", dirname(__FILE__));
 define("APP_DIR", WWW_DIR . "/app");
-define("TESTER_DIR", WWW_DIR . "/libs/mytester/src");
 
 require __DIR__ . "/vendor/autoload.php";
 Tracy\Debugger::enable(null, APP_DIR . "/log");
