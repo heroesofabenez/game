ab8fab8fc0f41b8a1420fb41d68858aa94e1c1ca
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-07-14 18:15:12 +0200

    used constant WWW_DIR in run_tests.php

--- run_tests.php
+++ run_tests.php
@@ -2,7 +2,7 @@
 define("WWW_DIR", dirname(__FILE__));
 define("APP_DIR", WWW_DIR . "/app");
 
-require __DIR__ . "/vendor/autoload.php";
+require WWW_DIR . "/vendor/autoload.php";
 Tracy\Debugger::enable(null, APP_DIR . "/log");
 
 $container = require APP_DIR . "/bootstrap.php";
