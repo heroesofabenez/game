f498220be66d00c7bcca0cef10693c174036297d
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-07-24 10:33:24 +0200

    added descriptions of skills

---
+++ app/presenters/SkillPresenter.php
@@ -0,0 +1,54 @@
+<?php
+namespace HeroesofAbenez\Presenters;
+
+/**
+ * Presenter Skill
+ *
+ * @author Jakub Konečný
+ */
+class SkillPresenter extends BasePresenter {
+  /** @var \HeroesofAbenez\Model\Skills @autowire */
+  protected $model;
+  
+  /**
+   * @return void
+   */
+  protected function startup() {
+    parent::startup();
+    if($this->action != "default") {
+      $this->template->level = $this->user->identity->level;
+      $this->template->class = $this->user->identity->occupation;
+    }
+  }
+  
+  /**
+   * Page /skill does not exist
+   * 
+   * @return void
+   * @throws \Nette\Application\BadRequestException
+   */
+  function actionDefault() {
+    throw new \Nette\Application\BadRequestException;
+  }
+  
+  /**
+   * @param int $id
+   * @return void
+   */
+  function renderAttack($id) {
+    $skill = $this->model->getAttackSkill($id);
+    if(!$skill) $this->forward("notfound");
+    $this->template->skill = $skill;
+  }
+  
+  /**
+   * @param int $id
+   * @return void
+   */
+  function renderSpecial($id) {
+    $skill = $this->model->getSpecialSkill($id);
+    if(!$skill) $this->forward("notfound");
+    $this->template->skill = $skill;
+  }
+}
+?>
