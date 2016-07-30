180858f86e8e116bdefb5278a7d60b8437ca7948
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-07-30 11:11:59 +0100

    used constant expression

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 const WWW_DIR = __DIR__;
-define("APP_DIR", WWW_DIR . "/app");
+const APP_DIR = WWW_DIR . "/app";
 
 $container = require APP_DIR . "/bootstrap.php";
 $container->getService("application")->run();
