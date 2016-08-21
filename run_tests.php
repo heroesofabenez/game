c882259acd37a02bcdcffad56d3aa16654946c2b
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-08-21 12:50:47 +0100

    try to login the user in run_tests.php

--- run_tests.php
+++ run_tests.php
@@ -6,6 +6,12 @@ require WWW_DIR . "/vendor/autoload.php";
 Tracy\Debugger::enable(null, APP_DIR . "/log");
 
 $container = require APP_DIR . "/bootstrap.php";
+$user = $container->getService("security.user");
+if(!$user->isLoggedIn()) $user->login();
+if($user->id === 0) {
+  echo "Cannot login. Exiting.";
+  exit(255);
+}
 $result = $container->getService("mytester.runner")->execute();
 exit((int) $result);
 ?>
