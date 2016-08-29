84eb05b477ba587e2d09088781d330bab360c1ca
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-08-29 18:17:42 +0100

    (probaly) fixed setup_ci.php

--- setup_ci.php
+++ setup_ci.php
@@ -1,11 +1,11 @@
 <?php
 use Nette\Neon\Neon;
+use Nette\Utils\Finder;
 
 const WWW_DIR = __DIR__;
 const APP_DIR = WWW_DIR . "/app";
 
 require WWW_DIR . "/vendor/autoload.php";
-Tracy\Debugger::enable(null, APP_DIR . "/log");
 
 $filename = APP_DIR . "/config/local.neon";
 $cfg = Neon::decode(file_get_contents($filename));
@@ -16,8 +16,8 @@ file_put_contents($filename, Neon::encode($cfg, Neon::BLOCK));
 /*$container = require APP_DIR . "/bootstrap.php";
 $connection = $container->getByType(\Nette\Database\Connection::class);
 
-$sqlFiles = ["structure.sql", "data_basic.sql", "data_test.sql"];
-foreach($sqlFiles as $file) {
-  \Nette\Database\Helpers::loadFromFile($connection, __DIR__ . "/app/sqls/" . $file);
+$files = Finder::findFiles("*.sql")->in(WWW_DIR . "/sqls");
+foreach($files as $file) {
+  Nette\Database\Helpers::loadFromFile($conn, $file);
 }*/
 ?>
