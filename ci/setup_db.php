40deccef6b5b44c5750981c5b47feb030bfcbaf2
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-05-16 12:00:36 +0200

    announce executed files in ci/setup_db.php

--- ci/setup_db.php
+++ ci/setup_db.php
@@ -10,6 +10,8 @@ $connection = new \Nette\Database\Connection($dbConfig["dsn"], $dbConfig["user"]
 $sqlsFolder = __DIR__ . "/../app/sqls";
 $files = ["structure", "data_basic", "data_test"];
 foreach($files as $file) {
+  echo "Executing file: $file";
   Nette\Database\Helpers::loadFromFile($connection, "$sqlsFolder/$file.sql");
+  echo "... Done";
 }
 ?>
