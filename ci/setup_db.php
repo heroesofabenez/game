f5e16d5988612c6b146ca269ddc2070fbfa3218f
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-07-27 12:03:33 +0200

    show execution time in ci/setup_db.php

--- ci/setup_db.php
+++ ci/setup_db.php
@@ -6,6 +6,8 @@ use Nette\Neon\Neon,
 
 require __DIR__ . "/../vendor/autoload.php";
 
+Tracy\Debugger::timer("setup_db");
+
 $config = Neon::decode(file_get_contents(__DIR__ . "/../tests/local.neon"));
 
 $connection = new Nextras\Dbal\Connection($config["dbal"]);
@@ -13,7 +15,12 @@ $sqlsFolder = __DIR__ . "/../app/sqls";
 $files = ["structure", "data_basic", "data_test"];
 foreach($files as $file) {
   echo "Executing file: $file.sql";
+  Tracy\Debugger::timer($file);
   FileImporter::executeFile($connection, "$sqlsFolder/$file.sql");
-  echo " ... Done\n";
+  $time = round(Tracy\Debugger::timer($file), 2);
+  echo "Done in $time second(s)\n";
 }
+
+$time = round(Tracy\Debugger::timer("setup_db"), 2);
+echo "\nTotal time: $time second(s)\n";
 ?>
