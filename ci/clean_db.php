dcca6ac60d7629f6339733a8c7defaec80625ca0
Jakub Konečný <jakub.konecny2@centrum.cz>
2018-02-22 13:43:24 +0100

    use clean database for tests

---
+++ ci/clean_db.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use Nette\Neon\Neon,
+  Nextras\Dbal\QueryException;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$config = Neon::decode(file_get_contents(__DIR__ . "/../tests/local.neon"));
+
+$connection = new Nextras\Dbal\Connection($config["dbal"]);
+
+try {
+  $connection->query("SET foreign_key_checks = 0");
+  /** @var \Nextras\Dbal\Result\Result $tables */
+  $tables = $connection->query("SHOW TABLES");
+  while($table = $tables->fetchField(0)) {
+    $connection->query("DROP TABLE $table");
+  }
+} catch(QueryException $e) { // @codingStandardsIgnoreLine
+
+}
+?>
