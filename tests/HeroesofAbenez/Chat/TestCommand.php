010bc11689c5aedae63f46c926617d90b846f275
Jakub Konečný <jakub.konecny2@centrum.cz>
2018-03-17 15:44:23 +0100

    do not set value of TestCommand::$name in constructor, change the definition

--- tests/HeroesofAbenez/Chat/TestCommand.php
+++ tests/HeroesofAbenez/Chat/TestCommand.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 namespace HeroesofAbenez\Chat;
 
 final class TestCommand extends ChatCommand {
-  public function __construct() {
-    $this->name = ChatCommandsProcessorTest::COMMAND_NAME;
-  }
+  protected $name = ChatCommandsProcessorTest::COMMAND_NAME;
   
   public function execute(): string {
     return "passed";
