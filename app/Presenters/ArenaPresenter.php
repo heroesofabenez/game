216dd0622372d9ecaa5f49bab699e3701ee8ea72
Jakub Konečný <jakub.konecny2@centrum.cz>
2018-12-26 14:13:27 +0100

    do not allow players bellow level 3 to enter arena

--- app/Presenters/ArenaPresenter.php
+++ app/Presenters/ArenaPresenter.php
@@ -12,8 +12,16 @@ use HeroesofAbenez\Arena;
  */
 final class ArenaPresenter extends BasePresenter {
   /** @var int */
-  protected $min_level = 3;
-  
+  protected $minLevel = 3;
+
+  protected function startup(): void {
+    parent::startup();
+    if($this->user->identity->level < $this->minLevel) {
+      $this->flashMessage($this->translator->translate("errors.arena.lowLevel", $this->minLevel));
+      $this->redirect("Homepage:");
+    }
+  }
+
   public function renderChampion(int $id): void {
     $this->template->id = $id;
   }
